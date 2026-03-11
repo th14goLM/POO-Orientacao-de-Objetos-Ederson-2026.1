a5a77ff861d4594373e6235a54f1e7845187445f
ALUNO <thyagodelimamachado@gmail.com>
2026-03-11 10:12:31 -0300

    11/03 - Arquivo de Abstração

---
+++ Abstration/src/C_Abstracao.java
@@ -0,0 +1,10 @@
+// Exemplo de Abstração
+// Definição de uma abstração chamada de forma de pagamento
+// As classes concretas serão pix, cartão de credito, implementar o como pagar
+// O sistema de checkout depende da abstração, não das classes concretas
+
+// Isso vai facilitar novas formas de pagamento sem alterar o checkout
+
+public class C_Abstracao{
+
+}
