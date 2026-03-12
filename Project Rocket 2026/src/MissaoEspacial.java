6ecbb07cd26195b49b9262a6ba2922d00aab8c25
ALUNO <thyagodelimamachado@gmail.com>
2026-03-12 10:31:06 -0300

    12/03 - Update Missão espacial

--- Project Rocket 2026/src/MissaoEspacial.java
+++ Project Rocket 2026/src/MissaoEspacial.java
@@ -7,7 +7,7 @@ void main() {
 
     Satelite sputnik = new Satelite("Sputnik", 250, 50, "A ser definida!","Espionagem");
     Satelite hubble = new Satelite("Hubble", 400, 25, "GEO", "Científico");
-    Satelite jamesWebb = new Satelite("JamesWebb", 850, 0, "Órbita Lunar", "Comunicação");
+    Satelite jamesWebb = new Satelite("JamesWebb", 850, 20, "Órbita Lunar", "Comunicação");
 
     // Chamada para adicionar os Foguetes
     controle.adicionarFoguete(falcon);
