c13d89a101a168750c389349ae28f8d615873256
Thiago <thyagodelimamachado@gmail.com>
2026-03-22 00:50:19 -0300

    22/03 - Pequenas melhorias e introdução de novas funções

--- Projeto Folha de Pagamento/src/FolhaPagamento.java
+++ Projeto Folha de Pagamento/src/FolhaPagamento.java
@@ -1,5 +1,6 @@
 public class FolhaPagamento {
 
+    /** Método para o calculo do salário dos funcionários  **/
     public Holerite calcularSalario(ContratoTrabalho contrato, int horasNormais, int horasExtras) {
 
         double valorHoraNormal = contrato.getValorHoraNormal();
@@ -10,12 +11,11 @@ public class FolhaPagamento {
 
         double adicionalFilhos = 0.0;
 
+        /** Adicional de pagamento para Pais e Mães  **/
         if (contrato.temAdicional()){
             adicionalFilhos = (valorTotalNormal + valorTotalExtra) * 0.10;
         }
 
-        Holerite holeriteGerado = new Holerite(contrato.getFuncionario(),valorTotalNormal,valorTotalExtra,adicionalFilhos);
-
-        return holeriteGerado;
+        return new Holerite(contrato.getFuncionario(),valorTotalNormal,valorTotalExtra,adicionalFilhos);
     }
 }
